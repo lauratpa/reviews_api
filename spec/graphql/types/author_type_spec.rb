f1aaaaffbd6c3d598447c6d0563b79fd7af50e8e
Laura Paakkinen <la.paakkinen@gmail.com>
2020-09-08 16:38:17 +0200

    Add Author model and QueryType

---
+++ spec/graphql/types/author_type_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe Types::QueryType do
+  describe "authors" do
+    let(:query) do
+      %(query {
+        authors {
+          id
+          name
+        }
+      })
+    end
+
+
+    it "returns all items" do
+      authors = [
+        Author.create!(name: "Joe"),
+        Author.create!(name: "Jill"),
+      ]
+      result = ReviewsApiSchema.execute(query).as_json
+
+      expect(result.dig("data", "authors")).to match_array(
+        authors.map { |author| { "id" => author.id.to_s, "name" => author.name }}
+      )
+    end
+  end
+end
